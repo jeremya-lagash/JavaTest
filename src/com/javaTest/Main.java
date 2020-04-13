eb71dc82471d3a25e7f2fa9d97c1dd0fa58ae98a
jeremya-lagash <jeremya@lagash.com>
2020-04-13 11:31:50 -0400

    avance 11:20

--- src/com/javaTest/Main.java
+++ src/com/javaTest/Main.java
@@ -1,7 +1,25 @@
 package com.javaTest;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello test");
+        doTryWithResourcesMulti();
+    }
+
+    public static void doTryWithResourcesMulti() {
+        char[] buff = new char[50];
+        int length;
+        try (Reader reader = Helper.openReader("file.txt");
+             Writer writer = Helper.openWriter("jeremy.txt"))  {
+            while((length = reader.read(buff)) >= 0) {
+                System.out.println("\nlength: " + length);
+                writer.write(buff, 0, length);
+            }
+        } catch(IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
     }
 }
