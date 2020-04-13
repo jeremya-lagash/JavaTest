f669aa703610c5e4813fa5c5d85060482a4446a9
Javier  Calderon <33453799+jcalderonseguel@users.noreply.github.com>
2020-04-13 11:25:09 -0400

    Ejercicio Resuelto

--- src/com/javaTest/Helper.java
+++ src/com/javaTest/Helper.java
@@ -1,4 +1,27 @@
 package com.javaTest;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 public class Helper {
+
+    static public Reader openReader(String fileName) throws IOException {
+        return Files.newBufferedReader(Paths.get(fileName));
+    }
+
+    static public Writer openWriter(String fileName) throws IOException {
+        return Files.newBufferedWriter(Paths.get(fileName));
+    }
+    //[Z,T,R,O,H,G,I,D,..Q]
+    //[Character,Character,Character,Character,Character,Character,Character,Character,..Character]
+    static public ArrayList<Character> sortAscending(ArrayList<Character> arrayList) {
+        Collections.sort(arrayList);
+        return arrayList;
+    }
 }
--- src/com/javaTest/Main.java
+++ src/com/javaTest/Main.java
@@ -1,7 +1,42 @@
 package com.javaTest;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello test");
+        doGetStringOrderAndConsonant();
+    }
+
+    public static void doGetStringOrderAndConsonant() {
+        char[] buff = new char[30];
+        ArrayList<Character> pivotArray = new ArrayList();
+        int length;
+        try (Reader reader = Helper.openReader("file.txt");
+             Writer writer = Helper.openWriter("master.txt"))  {
+            while((length = reader.read(buff)) >= 0) {
+                for(int i=0; i < length; i++) {
+                    pivotArray.add(buff[i]) ;
+                }
+
+            }
+            //ZTROHGIDPYSAWJKCFNLMXUBEVQ
+            //[A,B,C,D,E,F,G....Z] => Helper.sortAscending(pivotArray)
+            for (Character i : Helper.sortAscending(pivotArray)) {
+                //[Z,T,R,O,H,G,I,D,..Q]
+                if (!i.toString().equals("A") && !i.toString().equals("E") && !i.toString().equals("I") && !i.toString().equals("O") && !i.toString().equals("U")) {
+                    int index =1;
+                    writer.write(i.toString(), 0, index);
+                    index ++;
+
+                }
+            }
+
+
+        } catch(IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
     }
 }
